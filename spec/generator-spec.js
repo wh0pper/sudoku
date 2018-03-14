d8ba15cb2af8db731e7a8c9939ec6ce427e05d95
Emily Watkins and Jared Clemmensen <jared@>
2018-03-14 16:56:19 -0700

    add method for nextRow, not passing

--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -2,11 +2,23 @@ import {Generator} from './../src/generator.js';
 import Helper from './../src/helper';
 
 describe('Generator', function() {
+var testGrid;
+
+  beforeEach(function() {
+    testGrid = new Generator();
+  });
+
 
   it('should return an array with items 1-9', function() {
-    var row = new Generator();
-    expect(row.randomRow().length).toEqual(9);
-    var new_row = row.randomRow();
+    expect(testGrid.randomRow().length).toEqual(9);
+    var new_row = testGrid.randomRow();
     expect(Helper.arrayCompare(new_row)).toEqual(true);
+  });
+
+  it('should return a second row with items 1-9, compatible with first row', function() {
+    var row1 = testGrid.randomRow();
+    var row2 = testGrid.nextRow();
+    expect(row2.length).toEqual(9);
+    expect(Helper.arrayCompare(row2)).toEqual(true);
   });
 });
--- src/generator.js
+++ src/generator.js
@@ -10,7 +10,30 @@ export class Generator {
       var item = bank.splice(Math.floor(Math.random()*bank.length), 1)
       row.push(item[0]);
     }
+    this.grid.push(row);
     return row;
   }
 
+  nextRow() {
+    var row = [];
+    var currentRowIndex = this.grid.length;
+    var previousRow = this.grid[currentRowIndex-1];
+
+    var bank = [1,2,3,4,5,6,7,8,9];
+    for (var j=0; j < 9; j++) {
+      //loop through corresponding three of previous rows and remove from bank
+      var i = Math.floor(j/3)*3;
+      var iMax = i + 3;
+      for (i; i < iMax; i++) {
+        var indexRemove = bank.indexOf(previousRow[i]);
+        bank.splice(indexRemove, 1);
+      }
+      var item = bank.splice(Math.floor(Math.random()*bank.length), 1)
+      row.push(item[0]);
+    }
+    this.grid.push(row);
+    console.log(previousRow);
+    console.log(row);
+    return row;
+  }
 }
