82be977df7112615dab2a67671e5be132d4209b7
Emily Watkins and Jared Clemmensen <jared@>
2018-03-15 11:39:57 -0700

    finish second row, realize weve made a huge mistake

--- src/generator.js
+++ src/generator.js
@@ -17,23 +17,44 @@ export class Generator {
   nextRow() {
     var row = [];
     var currentRowIndex = this.grid.length;
-    var previousRow = this.grid[currentRowIndex-1];
-
+    var prevRow = this.grid[currentRowIndex - 1];
     var bank = [1,2,3,4,5,6,7,8,9];
-    for (var j=0; j < 9; j++) {
-      //loop through corresponding three of previous rows and remove from bank
-      var i = Math.floor(j/3)*3;
-      var iMax = i + 3;
-      for (i; i < iMax; i++) {
-        var indexRemove = bank.indexOf(previousRow[i]);
+    var prevTri = [];
+    //0-2
+    for (var i = 0; i < 3; i++) { //adds to row considering first prev triad
+      var numRemove = prevRow[i];
+      prevTri.push(numRemove);
+      var indexRemove = bank.indexOf(numRemove);
+      if (indexRemove !== -1) {
         bank.splice(indexRemove, 1);
       }
-      var item = bank.splice(Math.floor(Math.random()*bank.length), 1)
+    }
+    for (var k = 0; k < 3; k++) {
+      var item = bank.splice(Math.floor(Math.random()*bank.length), 1) //random from remaining bank
       row.push(item[0]);
     }
-    this.grid.push(row);
-    console.log(previousRow);
-    console.log(row);
-    return row;
+
+    bank = bank.concat(prevTri);
+    // 3-5
+    prevTri = [];
+    for (var j = 3; j < 6; j++) { //adds to row considering second prev triad
+      numRemove = prevRow[j];
+      prevTri.push(numRemove);
+      indexRemove = bank.indexOf(numRemove);
+      if (indexRemove !== -1) {
+        bank.splice(indexRemove, 1);
+      }
+    }
+    for (var l = 0; l < 3; l++) {
+      item = bank.splice(Math.floor(Math.random()*bank.length), 1) //random from remaining bank
+      row.push(item[0]);
+    }
+    prevTri.forEach(function(prevItem) {
+      if (row.includes(prevItem) === false) {
+        bank.push(prevItem);
+      }
+    })
+    //6-8
+    row = row.concat(bank);
   }
 }
